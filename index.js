f6f3d17d9cb2bba72c97c9fa24e52487cad2d42f
ahmadbrainworks <46571840+ahmadbrainworks@users.noreply.github.com>
2019-03-05 20:58:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+//Javascript Created by https://fb.me/ahmadbrainworks  
+
+//store the quotations in arrays
+var images = [], index = 0; images[0] = "<a href = "http://bayanai.com"/><img src='https://lh3.googleusercontent.com/-rp0uROfYUug/XH7PwmKZFXI/AAAAAAAAB1o/5V-PGE3h-_03wwB7cWZQqQg4_AAb5dv5ACHMYCw/s640/%255BUNSET%255D"alt='Visit Bayanai.com'></a>";
+images[1] = "<a href = 'http://bayanai.com><img src='https://lh3.googleusercontent.com/-rp0uROfYUug/XH7PwmKZFXI/AAAAAAAAB1o/5V-PGE3h-_03wwB7cWZQqQg4_AAb5dv5ACHMYCw/s640/%255BUNSET%255D' alt='Visit Bayanai.com></a>";
+images[2] = "<a href = 'http://bayanai.com'><img src='https://lh3.googleusercontent.com/-ej4HurNnDCM/XH7PxA5nAVI/AAAAAAAAB1s/0FpdIjCXFEMaK-jvy2GzZcROf8nJiI2WACHMYCw/s640/%255BUNSET%255D' alt='Visit Bayanai.com'></a>";
+index = Math.floor(Math.random() * images.length); document.write(images[index]);
+//done
+</script>
