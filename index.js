ea4157e02064203377851a5e3db0cfbf73b08f33
ahmadbrainworks <46571840+ahmadbrainworks@users.noreply.github.com>
2019-03-05 21:16:22 +0100

    Update index.js
    edited

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 <script type="text/javascript">
-//Javascript Created by https://fb.me/ahmadbrainworks  
-
-//store the quotations in arrays
-var images = [], index = 0;
-images[0] = "<a href = 'http://bayanai.com><img src='https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_201847_0001.png'alt='Visit Bayanai.com'></a>";
-images[1] = "<a href = 'http://bayanai.com><img src='https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_201847_0001.png' alt='Visit Bayanai.com></a>";
-images[2] = "<a href = 'http://bayanai.com'><img src='https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_202959_0001.png' alt='Visit Bayanai.com'></a>";
-index = Math.floor(Math.random() * images.length); document.write(images[index]);
-//done
+var total_images = 3;
+var random_number = Math.floor((Math.random()*total_images));
+var random_img = new Array();
+random_img[0] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_202959_0001.png"></a>';
+random_img[1] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_202959_0001.png"></a>';
+random_img[2] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_201847_0001.png"></a>';
+document.write(random_img[random_number]);
 </script>
