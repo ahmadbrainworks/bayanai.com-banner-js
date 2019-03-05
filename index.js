8ec2987c0bc769aa76f29d2a020a359284c53ec0
ahmadbrainworks <46571840+ahmadbrainworks@users.noreply.github.com>
2019-03-05 21:05:59 +0100

    Update index.js
    bayanai js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 //Javascript Created by https://fb.me/ahmadbrainworks  
 
 //store the quotations in arrays
-var images = [], index = 0; images[0] = "<a href = "http://bayanai.com"/><img src='https://lh3.googleusercontent.com/-rp0uROfYUug/XH7PwmKZFXI/AAAAAAAAB1o/5V-PGE3h-_03wwB7cWZQqQg4_AAb5dv5ACHMYCw/s640/%255BUNSET%255D"alt='Visit Bayanai.com'></a>";
-images[1] = "<a href = 'http://bayanai.com><img src='https://lh3.googleusercontent.com/-rp0uROfYUug/XH7PwmKZFXI/AAAAAAAAB1o/5V-PGE3h-_03wwB7cWZQqQg4_AAb5dv5ACHMYCw/s640/%255BUNSET%255D' alt='Visit Bayanai.com></a>";
-images[2] = "<a href = 'http://bayanai.com'><img src='https://lh3.googleusercontent.com/-ej4HurNnDCM/XH7PxA5nAVI/AAAAAAAAB1s/0FpdIjCXFEMaK-jvy2GzZcROf8nJiI2WACHMYCw/s640/%255BUNSET%255D' alt='Visit Bayanai.com'></a>";
+var images = [], index = 0;
+images[0] = "<a href = 'http://bayanai.com><img src='https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_201847_0001.png'alt='Visit Bayanai.com'></a>";
+images[1] = "<a href = 'http://bayanai.com><img src='https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_201847_0001.png' alt='Visit Bayanai.com></a>";
+images[2] = "<a href = 'http://bayanai.com'><img src='https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_202959_0001.png' alt='Visit Bayanai.com'></a>";
 index = Math.floor(Math.random() * images.length); document.write(images[index]);
 //done
 </script>
