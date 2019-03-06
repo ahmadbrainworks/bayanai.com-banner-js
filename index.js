f7b58bfa66755427fd18a6cf87d9f17860e000b4
ahmadbrainworks <46571840+ahmadbrainworks@users.noreply.github.com>
2019-03-06 02:06:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var total_images = 3;
 var random_number = Math.floor((Math.random()*total_images));
 var random_img = new Array();
-random_img[0] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_202959_0001.png"></a>';
-random_img[1] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_202959_0001.png"></a>';
+random_img[0] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190306_015656_0001.png"></a>';
+random_img[1] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/FB_IMG_15518339644660813.jpg"></a>';
 random_img[2] = '<a href="http://bayanai.com"><img src="https://raw.githubusercontent.com/ahmadbrainworks/bayanai.com-banner-js/master/20190305_201847_0001.png"></a>';
 document.write(random_img[random_number]);
 </script>
